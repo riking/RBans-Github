6ea23a411c391209ff5f00892391cb82a11bfe75
Robert Trainor <rastro1337@gmail.com>
2012-08-22 16:50:32 -0400

    Updated To Latest Bukkit Build
    Updated To Latest Bukkit Build

--- src/net/zxq/rastrosgonegriefing/rbans/RBans.java
+++ src/net/zxq/rastrosgonegriefing/rbans/RBans.java
@@ -1,51 +1,33 @@
 package net.zxq.rastrosgonegriefing.rbans;
 
 import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Properties;
 import java.util.logging.Logger;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-
 import net.zxq.rastrosgonegriefing.commands.BanExecutor;
-import net.zxq.rastrosgonegriefing.commands.JFrameExecutor;
 import net.zxq.rastrosgonegriefing.commands.KickExecutor;
 import net.zxq.rastrosgonegriefing.commands.MuteExecutor;
 import net.zxq.rastrosgonegriefing.commands.PlayerInfoExecutor;
 import net.zxq.rastrosgonegriefing.commands.RollbackBanExecutor;
-import net.zxq.rastrosgonegriefing.commands.RollbackExecutor;
 import net.zxq.rastrosgonegriefing.commands.TempBanExecutor;
 import net.zxq.rastrosgonegriefing.commands.UnBanExecutor;
 import net.zxq.rastrosgonegriefing.commands.UnMuteExecutor;
 import net.zxq.rastrosgonegriefing.listeners.PlayerLoginListener;
 import net.zxq.rastrosgonegriefing.listeners.RBansPlayerListener;
-import net.zxq.rastrosgonegriefing.rframe.RFrame;
 import net.zxq.rastrosgonegriefing.util.BlockDestroyListStore;
 import net.zxq.rastrosgonegriefing.util.BlockPlacedListStore;
 import net.zxq.rastrosgonegriefing.util.ListStore;
 import net.zxq.rastrosgonegriefing.util.MutedPlayersListStore;
 import net.zxq.rastrosgonegriefing.util.PlayerChatListStore;
 import net.zxq.rastrosgonegriefing.util.PlayerJoinListStore;
-import net.zxq.rastrosgonegriefing.util.RBPlayer;
 import net.zxq.rastrosgonegriefing.util.TempBanListStore;
 
 import org.bukkit.ChatColor;
-import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 public class RBans extends JavaPlugin {
 	public static Logger log = Logger.getLogger("Minecraft");
@@ -58,8 +40,6 @@ public class RBans extends JavaPlugin {
 	public TempBanListStore tban;
 	protected UpdateChecker uC;
 	public static HashMap<String, Long> banned = new HashMap<String, Long>();
-	public static HashMap<String, RBPlayer> players = new HashMap<String, RBPlayer>();
-	public static File file = new File("RBans.txt");
 	
 	@Override
 	public void onEnable() {
@@ -80,9 +60,9 @@ public class RBans extends JavaPlugin {
 			banned = new HashMap<String, Long>();
 		}
 
-		this.bannedPlayers = new ListStore(new File(pluginFolder
+		RBans.bannedPlayers = new ListStore(new File(pluginFolder
 				+ File.separator + "localbans.txt"));
-		this.bannedPlayers.loadFile();
+		RBans.bannedPlayers.loadFile();
 
 		this.brokenBlocks = new BlockDestroyListStore(new File(pluginFolder
 				+ File.separator + "Destroyed Blocks Log.txt"));
@@ -155,9 +135,7 @@ public class RBans extends JavaPlugin {
 		this.getCommand("mute").setExecutor(new MuteExecutor(this));
 		this.getCommand("unmute").setExecutor(new UnMuteExecutor(this));
 		this.getCommand("tempban").setExecutor(new TempBanExecutor(this));
-		this.getCommand("rframe").setExecutor(new JFrameExecutor(this));
 		this.getCommand("info").setExecutor(new PlayerInfoExecutor(this));
-		this.getCommand("rbans rb").setExecutor(new RollbackExecutor(this));
 
 		this.getServer().getScheduler()
 				.scheduleAsyncRepeatingTask(this, new Runnable() {
@@ -175,28 +153,10 @@ public class RBans extends JavaPlugin {
 						}
 					}
 				}, 240L, 800L);
-		
-		if (!file.exists()) {
-			try {
-				file.createNewFile();
-				log.info(ChatColor.BLUE + "No RBans file yet, creating one for you.");
-			} catch (IOException e) {
-				log.warning(e.getMessage());
-			}
-		}
-		
-		for(Player p : getServer().getOnlinePlayers()) {
-			// add the online players
-			players.put(p.getName(), new RBPlayer(this, p));
-		}
-		// add the console as GLPlayer
-		ConsoleCommandSender console = this.getServer().getConsoleSender();
-		players.put(console.getName(), new RBPlayer(this, console));
 	}
 
 	@Override
 	public void onDisable() {
-		players.clear();
 		log("Disabled");
 
 		bannedPlayers.saveFile();
@@ -208,6 +168,6 @@ public class RBans extends JavaPlugin {
 	}
 
 	public void log(String msg) {
-		this.log.info("[RBans] " + msg);
+		RBans.log.info("[RBans] " + msg);
 	}
 }
